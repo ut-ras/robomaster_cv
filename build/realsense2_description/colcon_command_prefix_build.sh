bf9839cb2d9f23923be5e483bfa723f41c04b4ab
eddieliao <eddieliao03@gmail.com>
2023-09-15 17:51:34 -0500

    Remove build, log, install directories

--- build/realsense2_description/colcon_command_prefix_build.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/eddie/Documents/GitHub/robomaster_cv/install/realsense2_camera_msgs/share/realsense2_camera_msgs/package.sh"
