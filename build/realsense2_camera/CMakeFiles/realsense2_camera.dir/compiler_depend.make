2e5493e85947b610a95901f326ec0b20699e50d2
eddieliao <eddieliao03@gmail.com>
2023-09-15 00:10:53 -0500

    Add realsense package
    Former-commit-id: b442d7aaa5b73342a0bd7689e9fa0ff17e0be73d

---
+++ build/realsense2_camera/CMakeFiles/realsense2_camera.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for realsense2_camera.
+# This may be replaced when dependencies are built.
---
+++ build/realsense2_camera/gtest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /opt/ros/humble/src/gtest_vendor
+# Build directory: /home/eddie/Documents/GitHub/robomaster_cv/build/realsense2_camera/gtest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
