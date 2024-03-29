46a3fa0843dd1f23bd42db52bf21da6a310953e1
Tanay Garg <tanay.garg@utexas.edu>
2024-03-29 18:02:17 -0500

    Kalman filter works (?)
    Former-commit-id: 813fceac9c652129e6f657969840acabae5595e8

--- src/object-log/Kalman.cu
+++ src/object-log/Kalman.cu
@@ -4,16 +4,25 @@
 #include <stdio.h>
 #include <iostream>
 
-void storeColumnMajor(float src[][VECTOR_SIZE], float *dest) {
+/*
+ * @param src: 2D array to be stored in column major format
+ * @param dest: pointer to the destination array
+ * @return void
+*/
+void Kalman::storeColumnMajor(float src[][VECTOR_SIZE], float *dest)
+{
     float *dest_copy = dest;
-    for(int i = 0; i < VECTOR_SIZE; i++){
+    for (int i = 0; i < VECTOR_SIZE; i++)
+    {
         // columns
-        for(int j = 0; j < VECTOR_SIZE; j++) {
+        for (int j = 0; j < VECTOR_SIZE; j++)
+        {
             // rows
-            *dest_copy++ = src[j][i]; 
+            *dest_copy++ = src[j][i];
         }
     }
 }
+
 Kalman::Kalman(float deltaTime)
 {
 
@@ -24,6 +33,9 @@ Kalman::Kalman(float deltaTime)
         std::cout << "CUBLAS initialization failed!" << std::endl;
         return;
     }
+
+    _state_n_1 = (float *)calloc(sizeof(float *), VECTOR_SIZE);
+    _state_n = (float *)calloc(sizeof(float *), VECTOR_SIZE);
 
     float time_factor = 0.5f * deltaTime * deltaTime;
     float _state_transition_matrix_init[][VECTOR_SIZE] = {
@@ -37,12 +49,9 @@ Kalman::Kalman(float deltaTime)
         {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0},
         {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0},
     };
-    _state_transition_matrix = (float *) malloc(VECTOR_SIZE * VECTOR_SIZE * sizeof(float*));
+    _state_transition_matrix = (float *)malloc(VECTOR_SIZE * VECTOR_SIZE * sizeof(float *));
     storeColumnMajor(_state_transition_matrix_init, _state_transition_matrix);
 
-    // for(int i = 0; i < VECTOR_SIZE * VECTOR_SIZE; i++) {
-    //     std::cout<<_state_transition_matrix[i]<<" "<<std::endl;
-    // }
 };
 
 void Kalman::updateStateTransitionMatrix(float *matrix, float deltaTime)
@@ -58,12 +67,78 @@ void Kalman::updateStateTransitionMatrix(float *matrix, float deltaTime)
         {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0},
         {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0},
     };
-    
-    storeColumnMajor(_state_transition_matrix_init, matrix);
 
+    // stores the newly initialized matrix in column major format
+    storeColumnMajor(_state_transition_matrix_init, matrix);
+}
+
+void Kalman::set_state_n(float *position, float *velocity, float *acceleration)
+{
+    memcpy(_state_n, position, STATE_SIZE * sizeof(float));
+    memcpy(_state_n + STATE_SIZE, velocity, STATE_SIZE * sizeof(float));
+    memcpy(_state_n + 2 * STATE_SIZE, acceleration, STATE_SIZE * sizeof(float));
+}
+
+void Kalman::predict_state_n_1() {
+    // device pointers for all the vectors and matrices
+    float *d_state_transition_matrix, *d_state_n, *d_state_n_1;
+
+    cudaMalloc((void **)&d_state_transition_matrix, VECTOR_SIZE * VECTOR_SIZE * sizeof(float));
+    cudaMalloc((void **)&d_state_n, VECTOR_SIZE * sizeof(float));
+    cudaMalloc((void **)&d_state_n_1, VECTOR_SIZE * sizeof(float));
+
+    cublasStatus_t copy_status;
+    copy_status = cublasSetMatrix(VECTOR_SIZE, VECTOR_SIZE, sizeof(float), _state_transition_matrix, VECTOR_SIZE, d_state_transition_matrix, VECTOR_SIZE);
+    if (copy_status != CUBLAS_STATUS_SUCCESS)
+    {
+        std::cout << "Error copying state transition matrix to device" << std::endl;
+        return;
+    }
+
+    copy_status = cublasSetVector(VECTOR_SIZE, sizeof(float), _state_n, 1, d_state_n, 1);
+    if (copy_status != CUBLAS_STATUS_SUCCESS)
+    {
+        std::cout << "Error copying state_n to device" << std::endl;
+        return;
+    }
+
+    // perform matrix multiplication
+    float alpha = 1.0f;
+    float beta = 0.0f;
+    cublasSgemv(_handle, CUBLAS_OP_N, VECTOR_SIZE, VECTOR_SIZE, &alpha, d_state_transition_matrix, VECTOR_SIZE, d_state_n, 1, &beta, d_state_n_1, 1);
+
+    // copy the result back to host
+    copy_status = cublasGetVector(VECTOR_SIZE, sizeof(float), d_state_n_1, 1, _state_n_1, 1);
+    if (copy_status != CUBLAS_STATUS_SUCCESS)
+    {
+        std::cout << "Error copying state_n_1 to host" << std::endl;
+        return;
+    }
+
+}
+
+float *Kalman::get_state_n_1()
+{
+    return _state_n_1;
 }
 
 int main()
 {
-    Kalman *k = new Kalman(1.2);
+    /*
+     * TESTING
+    */
+    float DELTA_TIME = 1.2;
+    Kalman *k = new Kalman(DELTA_TIME);
+
+    float position[] = {1.0, 2.0, 3.0};
+    float velocity[] = {4.0, 5.0, 6.0};
+    float acceleration[] = {7.0, 8.0, 9.0};
+
+    k->set_state_n(position, velocity, acceleration);
+    k->predict_state_n_1();
+    float *output = k->get_state_n_1();
+    for (int i = 0; i < VECTOR_SIZE; i++)
+    {
+        std::cout << output[i] << std::endl;
+    }
 }
