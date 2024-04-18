a26ab695a8b0e2c602cd2943c887b5fbc4cca711
Eddie Liao <eddieliao03@gmail.com>
2024-04-18 18:07:28 -0500

    Add window positions; loop video

--- src/opencv/ClassicalFilter.cpp
+++ src/opencv/ClassicalFilter.cpp
@@ -6,17 +6,9 @@
 using namespace cv;
 using namespace std;
 
-int main( int argc, char** argv )
-{
-    VideoCapture cap("/Users/jeffreyzhou/Downloads/IMG_2129.mov"); //capture the video from webcam
-
-    if (!cap.isOpened() )  // if not success, exit program
-    {
-        cout << "Cannot open the web cam" << endl;
-        return -1;
-    }
-
+int main( int argc, char** argv ) {
     namedWindow("Control", WINDOW_AUTOSIZE); //create a window called "Control"
+    moveWindow("Control", 1620, 0);
 
     int iLowH = 1;
     int iHighH = 179;
@@ -28,96 +20,118 @@ int main( int argc, char** argv )
     int iHighV = 255;
 
     //Create trackbars in "Control" window
-    createTrackbar("LowH", "Control", &iLowH, iLowH); //Hue (0 - 179)
-    createTrackbar("HighH", "Control", &iHighH, iHighH);
+    createTrackbar("LowH", "Control", &iLowH, 179); //Hue (0 - 179)
+    createTrackbar("HighH", "Control", &iHighH, 179);
 
-    createTrackbar("LowS", "Control", &iLowS, iLowS); //Saturation (0 - 255)
-    createTrackbar("HighS", "Control", &iHighS, iHighS);
+    createTrackbar("LowS", "Control", &iLowS, 255); //Saturation (0 - 255)
+    createTrackbar("HighS", "Control", &iHighS, 255);
 
-    createTrackbar("LowV", "Control", &iLowV, iLowV);//Value (0 - 255)
-    createTrackbar("HighV", "Control", &iHighV, iHighV);
+    createTrackbar("LowV", "Control", &iLowV, 255);//Value (0 - 255)
+    createTrackbar("HighV", "Control", &iHighV, 255);
 
     int iLastX = -1; 
     int iLastY = -1;
 
-    //Capture a temporary image from the camera
-    Mat imgTmp;
-    cap.read(imgTmp); 
+    while (true) {
+        bool isClosed = 0;
+        // VideoCapture cap("../../resources/IMG_2129.MOV"); //capture the video from webcam
+        VideoCapture cap("../../resources/IMG_2127.MOV"); //capture the video from webcam
+        // VideoCapture cap(0);
 
-    //Create a black image with the size as the camera output
-
-    // Mat imgLines = Mat::zeros( imgTmp.size(), CV_8UC3 );;
-
-    while (true)
-    {
-        Mat imgOriginal;
-        Mat imgLines = Mat::zeros(imgTmp.size(), CV_8UC3);;
-        bool bSuccess = cap.read(imgOriginal); // read a new frame from video
-
-
-        if (!bSuccess) //if not success, break loop
+        if (!cap.isOpened() )  // if not success, exit program
         {
-            cout << "Cannot read a frame from video stream" << endl;
+            cout << "Cannot open the web cam" << endl;
+            return -1;
+        }
+
+        //Capture a temporary image from the camera
+        Mat imgTmp;
+        cap.read(imgTmp); 
+
+        while (true) {
+            Mat imgOriginal;
+            Mat imgLines = Mat::zeros(imgTmp.size(), CV_8UC3);;
+            bool bSuccess = cap.read(imgOriginal); // read a new frame from video
+
+            if (!bSuccess) //if not success, break loop
+            {
+                cout << "Cannot read a frame from video stream" << endl;
+                break;
+            }
+
+            Mat imgHSV;
+
+            cvtColor(imgOriginal, imgHSV, COLOR_BGR2HSV); //Convert the captured frame from BGR to HSV
+            
+            Mat imgThresholded;
+
+            inRange(imgHSV, Scalar(iLowH, iLowS, iLowV), Scalar(iHighH, iHighS, iHighV), imgThresholded); //Threshold the image
+            
+            //morphological opening (removes small objects from the foreground)
+            erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
+            dilate(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) ); 
+
+            //morphological closing (removes small holes from the foreground)
+            dilate(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) ); 
+            erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
+
+            Mat image_copy = imgOriginal.clone();
+            vector<vector<Point>> contours;
+            vector<Vec4i> hierarchy;
+            findContours(imgThresholded, contours, hierarchy, RETR_TREE, CHAIN_APPROX_NONE);
+            drawContours(image_copy, contours, -1, Scalar(0, 255, 0), 2);
+
+            namedWindow("Contours", WINDOW_AUTOSIZE); //create a window called "Contours"
+            moveWindow("Contours", 1080, 0);
+            resize(image_copy, image_copy, Size(540, 960));
+            imshow("Contours", image_copy);
+
+            //Calculate the moments of the thresholded image
+            Moments oMoments = moments(imgThresholded);
+
+            double dM01 = oMoments.m01;
+            double dM10 = oMoments.m10;
+            double dArea = oMoments.m00;
+
+            // if the area <= 10000, I consider that the there are no object in the image and it's because of the noise, the area is not zero 
+            // if (dArea > 10000)
+            // {
+            //     //calculate the position of the ball
+            //     int posX = dM10 / dArea;
+            //     int posY = dM01 / dArea;        
+                        
+            //     if (iLastX >= 0 && iLastY >= 0 && posX >= 0 && posY >= 0)
+            //     {
+            //         circle(imgLines, Point(posX, posY), 10, Scalar(0, 255, 0), FILLED, 2);
+            //     }
+
+            //     iLastX = posX;
+            //     iLastY = posY;
+            //     cout << "X, Y pos is " << posX << ", " << posY << "\n";
+            // }
+
+            imgOriginal = imgOriginal + imgLines;
+
+            namedWindow("Original", WINDOW_AUTOSIZE); //create a window called "Original"
+            moveWindow("Original", 0, 0);
+            resize(imgOriginal, imgOriginal, Size(540, 960));
+            imshow("Original", imgOriginal); //show the original image
+
+            namedWindow("Thresholded", WINDOW_AUTOSIZE); //create a window called "Thresholded"
+            moveWindow("Thresholded", 540, 0);
+            resize(imgThresholded, imgThresholded, Size(540, 960));
+            imshow("Thresholded", imgThresholded);
+
+            if (waitKey(30) == 27) //wait for 'esc' key press for 30ms. If 'esc' key is pressed, break loop
+            {
+                cout << "esc key is pressed by user" << endl;
+                isClosed = 1;
+                break; 
+            }
+        }
+        if (isClosed) {
             break;
         }
-
-        Mat imgHSV;
-
-        cvtColor(imgOriginal, imgHSV, COLOR_BGR2HSV); //Convert the captured frame from BGR to HSV
-        
-        Mat imgThresholded;
-
-        inRange(imgHSV, Scalar(iLowH, iLowS, iLowV), Scalar(iHighH, iHighS, iHighV), imgThresholded); //Threshold the image
-        
-        //morphological opening (removes small objects from the foreground)
-        erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
-        dilate(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) ); 
-
-        //morphological closing (removes small holes from the foreground)
-        dilate(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) ); 
-        erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
-
-        Mat image_copy = imgOriginal.clone();
-        vector<vector<Point>> contours;
-        vector<Vec4i> hierarchy;
-        findContours(imgThresholded, contours, hierarchy, RETR_TREE, CHAIN_APPROX_NONE);
-        drawContours(image_copy, contours, -1, Scalar(0, 255, 0), 2);
-        imshow("Contours", image_copy);
-
-        //Calculate the moments of the thresholded image
-        Moments oMoments = moments(imgThresholded);
-
-        double dM01 = oMoments.m01;
-        double dM10 = oMoments.m10;
-        double dArea = oMoments.m00;
-
-        // if the area <= 10000, I consider that the there are no object in the image and it's because of the noise, the area is not zero 
-        // if (dArea > 10000)
-        // {
-        //     //calculate the position of the ball
-        //     int posX = dM10 / dArea;
-        //     int posY = dM01 / dArea;        
-                    
-        //     if (iLastX >= 0 && iLastY >= 0 && posX >= 0 && posY >= 0)
-        //     {
-        //         circle(imgLines, Point(posX, posY), 10, Scalar(0, 255, 0), FILLED, 2);
-        //     }
-
-        //     iLastX = posX;
-        //     iLastY = posY;
-        //     cout << "X, Y pos is " << posX << ", " << posY << "\n";
-        // }
-
-        imgOriginal = imgOriginal + imgLines;
-        imshow("Original", imgOriginal); //show the original image
-        imshow("Thresholded", imgThresholded);
-
-        if (waitKey(30) == 27) //wait for 'esc' key press for 30ms. If 'esc' key is pressed, break loop
-        {
-            cout << "esc key is pressed by user" << endl;
-            break; 
-        }
     }
-
     return 0;
 }
--- src/opencv/cmake_install.cmake
+++ src/opencv/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/jeffreyzhou/Documents/RAS/robomaster_cv/src/opencv
+# Install script for directory: /home/eddie/Documents/GitHub/robomaster_cv/src/opencv
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -27,6 +27,11 @@ if(NOT CMAKE_INSTALL_COMPONENT)
   endif()
 endif()
 
+# Install shared libraries without execute permission?
+if(NOT DEFINED CMAKE_INSTALL_SO_NO_EXE)
+  set(CMAKE_INSTALL_SO_NO_EXE "1")
+endif()
+
 # Is this installation the result of a crosscompile?
 if(NOT DEFINED CMAKE_CROSSCOMPILING)
   set(CMAKE_CROSSCOMPILING "FALSE")
@@ -34,7 +39,7 @@ endif()
 
 # Set default install directory permissions.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/objdump")
+  set(CMAKE_OBJDUMP "/usr/bin/objdump")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
@@ -45,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/jeffreyzhou/Documents/RAS/robomaster_cv/src/opencv/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/eddie/Documents/GitHub/robomaster_cv/src/opencv/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
