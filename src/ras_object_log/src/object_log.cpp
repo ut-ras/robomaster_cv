9b8b7f3c4e7f5bbc7e7493dc643eb6942e4f3cda
Tanay Garg <tanay.garg@utexas.edu>
2024-05-24 03:05:47 -0500

    custom message type called input added
    Former-commit-id: 635fac69d3507139a8bd0da370bd59f63a8b4c41

---
+++ src/ras_object_log/src/object_log.cpp
@@ -0,0 +1,12 @@
+#include <cstdio>
+// #include "rclcpp/rclcpp.hpp"
+#include "object_log_msgs/msg/input.hpp"
+
+int main(int argc, char ** argv)
+{
+  (void) argc;
+  (void) argv;
+
+  printf("hello world ras_object_log package\n");
+  return 0;
+}
