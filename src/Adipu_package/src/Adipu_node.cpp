3d1b48addfeb6af2c9b5a5c4d7c730df275fd1c6
pulipakaaGH <pulipakaa24@greenhill.org>
2024-09-13 18:31:19 -0500

    spinning hello world

--- src/Adipu_package/src/Adipu_node.cpp
+++ src/Adipu_package/src/Adipu_node.cpp
@@ -1,10 +1,22 @@
 #include <cstdio>
+#include <rclcpp/rclcpp.hpp>
+
+class AdipuNode : public rclcpp::Node {
+
+public:
+  AdipuNode() : Node("Adipu_node") {
+    printf("hello world Adipu_package package\n");
+  }
+};
 
 int main(int argc, char ** argv)
 {
   (void) argc;
   (void) argv;
 
-  printf("hello world Adipu_package package\n");
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<AdipuNode>());
+  rclcpp::shutdown();
+  
   return 0;
 }
