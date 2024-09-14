bc537ca3f11df3816866222c98ced23512f4a62b
Jose Pedraza <jpedraza066@gmail.com>
2024-09-13 19:01:29 -0500

    successful build

--- jose_pedraza_package/src/jose_pedraza_node.cpp
+++ jose_pedraza_package/src/jose_pedraza_node.cpp
@@ -1,10 +1,21 @@
 #include <cstdio>
+#include <rclcpp/rclcpp.hpp>
+
+class jose_pedrazaNode : public rclcpp::Node {
+public:
+  jose_pedrazaNode() : Node("jose_pedrazaNode") {
+    printf("hello world jose_pedraza_package package\n");
+  }
+};
 
 int main(int argc, char ** argv)
 {
   (void) argc;
   (void) argv;
 
-  printf("hello world jose_pedraza_package package\n");
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<jose_pedrazaNode>());
+  rclcpp::shutdown();
+
   return 0;
 }
