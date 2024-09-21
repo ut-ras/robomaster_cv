00b931b552a2107424a7e6d6e7a57f2df4e54fa7
prajithv <prajith.velicheti@gmail.com>
2024-09-21 14:45:03 -0500

    change prajithv_node to spinning node

--- src/prajithv_package/src/prajithv_node.cpp
+++ src/prajithv_package/src/prajithv_node.cpp
@@ -1,10 +1,21 @@
 #include <cstdio>
+#include <rclcpp/rclcpp.hpp>
+
+class PrajithNode : public rclcpp::Node {
+  public:
+    PrajithNode() : Node("PrajithNode") {
+      printf("hello world prajithv_package package\n");
+    }
+};
 
 int main(int argc, char ** argv)
 {
   (void) argc;
   (void) argv;
 
-  printf("hello world prajithv_package package\n");
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<PrajithNode>());
+  rclcpp::shutdown();
+
   return 0;
 }
