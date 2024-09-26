a6b7a54bdd29090d969bd6c306ebefb0034248fb
prajithv <prajith.velicheti@gmail.com>
2024-09-26 20:36:41 +0000

    change prajithv_node to subscribe to image_raw topic

--- src/prajithv_package/src/prajithv_node.cpp
+++ src/prajithv_package/src/prajithv_node.cpp
@@ -1,11 +1,29 @@
 #include <cstdio>
 #include <rclcpp/rclcpp.hpp>
+#include <sensor_msgs/msg/image.hpp>
 
 class PrajithNode : public rclcpp::Node {
   public:
-    PrajithNode() : Node("PrajithNode") {
-      printf("hello world prajithv_package package\n");
+    PrajithNode() : Node("prajithv_node"), last_msg_{nullptr} {
+      sub_ = this->create_subscription<sensor_msgs::msg::Image>(
+        "image_raw",
+        rclcpp::QoS(1),
+        std::bind(&PrajithNode::prajithvCallback, this, std::placeholders::_1)
+      );
     }
+
+    void prajithvCallback(sensor_msgs::msg::Image::SharedPtr msg) {
+      last_msg_ = msg;
+      RCLCPP_INFO(
+        get_logger(), 
+        "Width: %d, Height: %d",
+        msg->width,
+        msg->height
+      );
+    }
+
+    rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr sub_;
+    sensor_msgs::msg::Image::SharedPtr last_msg_;
 };
 
 int main(int argc, char ** argv)
@@ -14,7 +32,12 @@ int main(int argc, char ** argv)
   (void) argv;
 
   rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<PrajithNode>());
+
+  auto node = std::make_shared<PrajithNode>();
+
+  RCLCPP_INFO(node->get_logger(), "prajithv node starting");
+
+  rclcpp::spin(node);
   rclcpp::shutdown();
 
   return 0;
