f5110f4863dd0085594c401adb9bf0ac1306f541
prajithv <prajith.velicheti@gmail.com>
2024-09-26 21:54:14 +0000

    change prajithv_node to convert ROS image to OpenCV image and apply affine transformation

--- src/prajithv_package/src/prajithv_node.cpp
+++ src/prajithv_package/src/prajithv_node.cpp
@@ -1,10 +1,24 @@
+/** 
+ * References:
+ * https://github.com/ut-ras/robomaster_cv/wiki/Onboarding
+ * http://wiki.ros.org/cv_bridge/Tutorials/UsingCvBridgeToConvertBetweenROSImagesAndOpenCVImages
+ * https://docs.opencv.org/4.x/d4/da8/group__imgcodecs.html#ga8ac397bd09e48851665edbe12aa28f25
+ * https://docs.opencv.org/4.x/d4/d61/tutorial_warp_affine.html
+*/ 
+
 #include <cstdio>
 #include <rclcpp/rclcpp.hpp>
 #include <sensor_msgs/msg/image.hpp>
+#include <cv_bridge/cv_bridge.h>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+
+using namespace cv;
 
 class PrajithNode : public rclcpp::Node {
   public:
-    PrajithNode() : Node("prajithv_node"), last_msg_{nullptr} {
+    PrajithNode() : Node("prajithv_node") {
       sub_ = this->create_subscription<sensor_msgs::msg::Image>(
         "image_raw",
         rclcpp::QoS(1),
@@ -13,17 +27,28 @@ class PrajithNode : public rclcpp::Node {
     }
 
     void prajithvCallback(sensor_msgs::msg::Image::SharedPtr msg) {
-      last_msg_ = msg;
-      RCLCPP_INFO(
-        get_logger(), 
-        "Width: %d, Height: %d",
-        msg->width,
-        msg->height
-      );
+      cv_bridge::CvImagePtr cv_image_ptr = cv_bridge::toCvCopy(msg);
+      Mat img = cv_image_ptr->image;
+
+      Point2f srcTri[3];
+      srcTri[0] = Point2f(0.f, 0.f);
+      srcTri[1] = Point2f(img.cols - 1.f, 0.f);
+      srcTri[2] = Point2f(0.f, img.rows - 1.f);
+
+      Point2f dstTri[3];
+      dstTri[0] = Point2f(0.f, img.rows * 0.5f);
+      dstTri[1] = Point2f(img.cols * 0.75f, 0.f);
+      dstTri[2] = Point2f(img.cols * 0.25f, img.rows - 1.f);
+
+      Mat warp_mat = getAffineTransform(srcTri, dstTri);
+      Mat warp_dst = Mat::zeros(img.rows, img.cols, img.type());
+
+      warpAffine(img, warp_dst, warp_mat, warp_dst.size());
+
+      imwrite("warped_image.jpg", warp_dst);
     }
 
     rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr sub_;
-    sensor_msgs::msg::Image::SharedPtr last_msg_;
 };
 
 int main(int argc, char ** argv)
