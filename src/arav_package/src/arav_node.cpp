43022708ce8a7e32f26bf14fe9afea5c7dc8b511
Arav-R <63694299+Arav-R@users.noreply.github.com>
2024-09-27 18:59:03 -0500

    working image size

--- src/arav_package/src/arav_node.cpp
+++ src/arav_package/src/arav_node.cpp
@@ -1,12 +1,22 @@
 #include <cstdio>
 #include <rclcpp/rclcpp.hpp>
-#include <memory>
+#include <sensor_msgs/msg/image.hpp>
+using std::placeholders::_1;
 
 class AravNode : public rclcpp::Node {
 public:
-  AravNode() : Node("arav_node") {
+  AravNode() : Node("AravNode") {
     printf("hello world arav_package package\n");
+    subscription_ = this->create_subscription<sensor_msgs::msg::Image>(
+    "image_raw", 10, std::bind(&AravNode::topic_callback, this, _1));
   }
+
+  private:
+    void topic_callback(const sensor_msgs::msg::Image & msg) const
+    {
+      RCLCPP_INFO(this->get_logger(), "I heard: '%d %d'", msg.width, msg.height);
+    }
+    rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr subscription_;
 };
 
 int main(int argc, char ** argv)
