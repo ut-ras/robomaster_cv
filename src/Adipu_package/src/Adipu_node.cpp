1370379db6a1aaff7e834c0c6cecce8c0fdd9ffd
pulipakaaGH <pulipakaa24@greenhill.org>
2024-09-27 13:46:42 -0500

    Subscriber node for image_raw works

--- src/Adipu_package/src/Adipu_node.cpp
+++ src/Adipu_package/src/Adipu_node.cpp
@@ -1,12 +1,24 @@
 #include <cstdio>
+#include <memory>
 #include <rclcpp/rclcpp.hpp>
+#include <sensor_msgs/msg/image.hpp>
+using std::placeholders::_1;
 
 class AdipuNode : public rclcpp::Node {
 
-public:
-  AdipuNode() : Node("Adipu_node") {
-    printf("hello world Adipu_package package\n");
-  }
+  public:
+    AdipuNode() : Node("Adipu_node") {
+      printf("Hello world Adipu_package package\n");
+      subscription_ = this->create_subscription<sensor_msgs::msg::Image>(
+        "image_raw", 10, std::bind(&AdipuNode::topic_callback, this, _1)
+      );
+    }
+  
+  private:
+    void topic_callback(const sensor_msgs::msg::Image::SharedPtr msg) const {
+      RCLCPP_INFO(this->get_logger(), "I heard: '%d' x '%d'", msg->width, msg->height);
+    }
+    rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr subscription_;
 };
 
 int main(int argc, char ** argv)
