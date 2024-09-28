857f1d52549320cd27e999d6003928b7697192f7
DevAggarwal01 <dev.aggarwal.student@gmail.com>
2024-09-28 00:15:18 +0000

    Spinning node

--- src/deva_package/src/deva_node.cpp
+++ src/deva_package/src/deva_node.cpp
@@ -1,4 +1,12 @@
 #include <cstdio>
+#include <rclcpp/rclcpp.hpp>
+
+class DevaNode : public rclcpp::Node {
+  public:
+    DevaNode() : Node("DevaNode") {
+      printf("hello world deva_package package\n");
+    }
+};
 
 int main(int argc, char ** argv)
 {
@@ -6,5 +14,10 @@ int main(int argc, char ** argv)
   (void) argv;
 
   printf("hello world deva_package package\n");
+
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<DevaNode>());
+  rclcpp::shutdown(); 
+
   return 0;
 }
