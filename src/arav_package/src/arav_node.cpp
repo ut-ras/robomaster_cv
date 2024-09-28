57eef12c32a539df63b20304875db6df5d7cc2cd
Arav-R <63694299+Arav-R@users.noreply.github.com>
2024-09-27 19:33:06 -0500

    working opencv!

--- src/arav_package/src/arav_node.cpp
+++ src/arav_package/src/arav_node.cpp
@@ -1,6 +1,9 @@
 #include <cstdio>
 #include <rclcpp/rclcpp.hpp>
 #include <sensor_msgs/msg/image.hpp>
+#include <cv_bridge/cv_bridge.h>
+#include <opencv2/opencv.hpp>
+
 using std::placeholders::_1;
 
 class AravNode : public rclcpp::Node {
@@ -11,12 +14,21 @@ public:
     "image_raw", 10, std::bind(&AravNode::topic_callback, this, _1));
   }
 
-  private:
-    void topic_callback(const sensor_msgs::msg::Image & msg) const
-    {
-      RCLCPP_INFO(this->get_logger(), "I heard: '%d %d'", msg.width, msg.height);
-    }
-    rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr subscription_;
+private:
+  void topic_callback(const sensor_msgs::msg::Image::SharedPtr msg) const
+  {
+        cv_bridge::CvImagePtr cv_ptr;
+        cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+        cv::Mat cv_image = cv_ptr->image;
+
+        cv::Mat gray_image;
+        cv::cvtColor(cv_image, gray_image, cv::COLOR_BGR2GRAY);
+        cv::imwrite("original_image.jpg", cv_image);
+        cv::imwrite("grayscale_image.jpg", gray_image);
+
+  }
+
+  rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr subscription_;
 };
 
 int main(int argc, char ** argv)
