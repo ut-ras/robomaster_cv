ab0e885a046a0ead8f0bbd60f9babc27e5b81ae6
DevAggarwal01 <dev.aggarwal.student@gmail.com>
2024-09-28 07:37:23 +0000

    Finished listening to a topic section

--- src/deva_package/src/deva_node.cpp
+++ src/deva_package/src/deva_node.cpp
@@ -1,11 +1,21 @@
 #include <cstdio>
 #include <rclcpp/rclcpp.hpp>
+#include <sensor_msgs/msg/image.hpp>
 
-class DevaNode : public rclcpp::Node {
+
+class DevANode : public rclcpp::Node {
   public:
-    DevaNode() : Node("DevaNode") {
-      printf("hello world deva_package package\n");
+    DevANode() : Node("DevA_Node") {
+      img_sub_ = this->create_subscription<sensor_msgs::msg::Image>(
+        "image_raw",
+        rclcpp::QoS(1),
+        std::bind(&DevANode::imageCallback, this, std::placeholders::_1)
+      );
     }
+    void imageCallback(sensor_msgs::msg::Image::SharedPtr msg) {
+      RCLCPP_INFO(get_logger(), "The height is %d and width is %d", msg->height, msg->width); 
+    }
+    rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr img_sub_;
 };
 
 int main(int argc, char ** argv)
@@ -16,7 +26,7 @@ int main(int argc, char ** argv)
   printf("hello world deva_package package\n");
 
   rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<DevaNode>());
+  rclcpp::spin(std::make_shared<DevANode>());
   rclcpp::shutdown(); 
 
   return 0;
