a9b52970882733ed3ed87c48832ebfc083b6c428
stampede <arthurzhang@utexas.edu>
2024-10-10 18:58:12 -0500

    allow running on host machine without building from source

--- scripts/run_dev.sh
+++ scripts/run_dev.sh
@@ -24,15 +24,20 @@ DOCKER_DIR="${ROOT}/../docker"
 
 BASE_NAME="robomaster-cv"
 IMAGE_NAME="$BASE_NAME-image"
-IMAGE_KEY="ros2_humble.realsense_source.tools.user"
 CONTAINER_NAME="$BASE_NAME-container"
 DEV_DIR="../"
 PLATFORM="$(uname -m)"
+MODEL="PC"
+if [ -f /proc/device-tree/model ] && [[ "$(cat /proc/device-tree/model)" =~ "Jetson Orin Nano" ]]; then
+    print_info "Detected Jetson Orin Nano, building librealsense from source"
+    IMAGE_KEY="ros2_humble.realsense_source.tools.user"
+    MODEL="JON"
+else
+    print_info "Did not detect Jetson Orin Nano, installing librealsense from package"
+    IMAGE_KEY="ros2_humble.realsense_pkg.tools.user"
+fi
 
-# Map host's display socket to docker
-# This should only happen for non-windows
-# DOCKER_ARGS+=("-v /tmp/.X11-unix:/tmp/.X11-unix")
-# DOCKER_ARGS+=("-v $HOME/.Xauthority:/home/admin/.Xauthority:rw")
+
 DOCKER_ARGS+=("-e DISPLAY")
 DOCKER_ARGS+=("-e NVIDIA_VISIBLE_DEVICES=all")
 DOCKER_ARGS+=("-e NVIDIA_DRIVER_CAPABILITIES=all")
@@ -41,32 +46,36 @@ DOCKER_ARGS+=("-e USER")
 
 # DOCKER_ARGS+=("-e FASTRTPS_DEFAULT_PROFILES_FILE=/usr/local/share/middleware_profiles/rtps_udp_profile.xml")
 
-# TODO @arthur find a way to determine jetson vs not jetson, potentially env variable
-# if [[ $PLATFORM == "aarch64" ]]; then
-#     DOCKER_ARGS+=("-v /usr/bin/tegrastats:/usr/bin/tegrastats")
-#     DOCKER_ARGS+=("-v /tmp/argus_socket:/tmp/argus_socket")
-#     DOCKER_ARGS+=("-v /usr/local/cuda-11.4/targets/aarch64-linux/lib/libcusolver.so.11:/usr/local/cuda-11.4/targets/aarch64-linux/lib/libcusolver.so.11")
-#     DOCKER_ARGS+=("-v /usr/local/cuda-11.4/targets/aarch64-linux/lib/libcusparse.so.11:/usr/local/cuda-11.4/targets/aarch64-linux/lib/libcusparse.so.11")
-#     DOCKER_ARGS+=("-v /usr/local/cuda-11.4/targets/aarch64-linux/lib/libcurand.so.10:/usr/local/cuda-11.4/targets/aarch64-linux/lib/libcurand.so.10")
-#     DOCKER_ARGS+=("-v /usr/local/cuda-11.4/targets/aarch64-linux/lib/libcufft.so.10:/usr/local/cuda-11.4/targets/aarch64-linux/lib/libcufft.so.10")
-#     DOCKER_ARGS+=("-v /usr/local/cuda-11.4/targets/aarch64-linux/lib/libnvToolsExt.so:/usr/local/cuda-11.4/targets/aarch64-linux/lib/libnvToolsExt.so")
-#     DOCKER_ARGS+=("-v /usr/local/cuda-11.4/targets/aarch64-linux/lib/libcupti.so.11.4:/usr/local/cuda-11.4/targets/aarch64-linux/lib/libcupti.so.11.4")
-#     DOCKER_ARGS+=("-v /usr/local/cuda-11.4/targets/aarch64-linux/lib/libcudla.so.1:/usr/local/cuda-11.4/targets/aarch64-linux/lib/libcudla.so.1")
-#     DOCKER_ARGS+=("-v /usr/local/cuda-11.4/targets/aarch64-linux/include/nvToolsExt.h:/usr/local/cuda-11.4/targets/aarch64-linux/include/nvToolsExt.h")
-#     DOCKER_ARGS+=("-v /usr/lib/aarch64-linux-gnu/tegra:/usr/lib/aarch64-linux-gnu/tegra")
-#     DOCKER_ARGS+=("-v /usr/src/jetson_multimedia_api:/usr/src/jetson_multimedia_api")
-#     DOCKER_ARGS+=("-v /opt/nvidia/nsight-systems-cli:/opt/nvidia/nsight-systems-cli")
-#     DOCKER_ARGS+=("--pid=host")
-#     DOCKER_ARGS+=("-v /opt/nvidia/vpi2:/opt/nvidia/vpi2")
-#     DOCKER_ARGS+=("-v /usr/share/vpi2:/usr/share/vpi2")
+if [[ $MODEL == "JON" ]]; then
+    # Map host's display socket to docker
+    # This should only happen for non-windows
+    DOCKER_ARGS+=("-v /tmp/.X11-unix:/tmp/.X11-unix") # TODO @arthur can probably forward for linux and/or macOS too
+    DOCKER_ARGS+=("-v $HOME/.Xauthority:/home/admin/.Xauthority:rw")
 
-#     # If jtop present, give the container access
-#     if [[ $(getent group jtop) ]]; then
-#         DOCKER_ARGS+=("-v /run/jtop.sock:/run/jtop.sock:ro")
-#         JETSON_STATS_GID="$(getent group jtop | cut -d: -f3)"
-#         DOCKER_ARGS+=("--group-add $JETSON_STATS_GID")
-#     fi
-# fi
+    DOCKER_ARGS+=("-v /usr/bin/tegrastats:/usr/bin/tegrastats")
+    DOCKER_ARGS+=("-v /tmp/argus_socket:/tmp/argus_socket")
+    DOCKER_ARGS+=("-v /usr/local/cuda-11.4/targets/aarch64-linux/lib/libcusolver.so.11:/usr/local/cuda-11.4/targets/aarch64-linux/lib/libcusolver.so.11")
+    DOCKER_ARGS+=("-v /usr/local/cuda-11.4/targets/aarch64-linux/lib/libcusparse.so.11:/usr/local/cuda-11.4/targets/aarch64-linux/lib/libcusparse.so.11")
+    DOCKER_ARGS+=("-v /usr/local/cuda-11.4/targets/aarch64-linux/lib/libcurand.so.10:/usr/local/cuda-11.4/targets/aarch64-linux/lib/libcurand.so.10")
+    DOCKER_ARGS+=("-v /usr/local/cuda-11.4/targets/aarch64-linux/lib/libcufft.so.10:/usr/local/cuda-11.4/targets/aarch64-linux/lib/libcufft.so.10")
+    DOCKER_ARGS+=("-v /usr/local/cuda-11.4/targets/aarch64-linux/lib/libnvToolsExt.so:/usr/local/cuda-11.4/targets/aarch64-linux/lib/libnvToolsExt.so")
+    DOCKER_ARGS+=("-v /usr/local/cuda-11.4/targets/aarch64-linux/lib/libcupti.so.11.4:/usr/local/cuda-11.4/targets/aarch64-linux/lib/libcupti.so.11.4")
+    DOCKER_ARGS+=("-v /usr/local/cuda-11.4/targets/aarch64-linux/lib/libcudla.so.1:/usr/local/cuda-11.4/targets/aarch64-linux/lib/libcudla.so.1")
+    DOCKER_ARGS+=("-v /usr/local/cuda-11.4/targets/aarch64-linux/include/nvToolsExt.h:/usr/local/cuda-11.4/targets/aarch64-linux/include/nvToolsExt.h")
+    DOCKER_ARGS+=("-v /usr/lib/aarch64-linux-gnu/tegra:/usr/lib/aarch64-linux-gnu/tegra")
+    DOCKER_ARGS+=("-v /usr/src/jetson_multimedia_api:/usr/src/jetson_multimedia_api")
+    DOCKER_ARGS+=("-v /opt/nvidia/nsight-systems-cli:/opt/nvidia/nsight-systems-cli")
+    DOCKER_ARGS+=("--pid=host")
+    DOCKER_ARGS+=("-v /opt/nvidia/vpi2:/opt/nvidia/vpi2")
+    DOCKER_ARGS+=("-v /usr/share/vpi2:/usr/share/vpi2")
+
+    # If jtop present, give the container access
+    if [[ $(getent group jtop) ]]; then
+        DOCKER_ARGS+=("-v /run/jtop.sock:/run/jtop.sock:ro")
+        JETSON_STATS_GID="$(getent group jtop | cut -d: -f3)"
+        DOCKER_ARGS+=("--group-add $JETSON_STATS_GID")
+    fi
+fi
 
 # Remove any exited containers.
 if [ "$(docker ps -a --quiet --filter status=exited --filter name=$CONTAINER_NAME)" ]; then
--- scripts/save_camera_bag.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-NOW=$( date '+%Y%m%d_%H%M%S' )
-ros2 bag record -e /robot --start-paused -o "$NOW-rosbag"
