11dcfb49bf2ccec5fe278d8b2bd37f1070ee1b0a
Eddie Liao <eddieliao03@gmail.com>
2025-01-24 18:49:29 -0600

    fix configure

--- configure.sh
+++ configure.sh
@@ -1,7 +1,7 @@
 ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source $ROOT/scripts/utils/print_color.sh
 
-if [ -f /proc/device-tree/model ] && [[ "$(cat /proc/device-tree/model)" =~ "Jetson Orin Nano" ]]; then
+if [ -f /proc/device-tree/model ] && [[ "$(cat /proc/device-tree/model)" =~ "Orin Nano" ]]; then
     print_info "Detected Jetson Orin Nano"
     cp docker/compose_files/docker-compose-jon.yml.example docker-compose.yml
     print_info "Copying docker-compose-jon.yml.example to docker-compose.yml"
