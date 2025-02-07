8e22903a7919285fc2dc692d58a539b52bd1b590
Dominic Paruolo <dominicparuolo78@gmail.com>
2025-02-07 17:58:06 -0600

    DominicNode

--- src/dominic_package/src/dominic_node.cpp
+++ src/dominic_package/src/dominic_node.cpp
@@ -1,10 +1,24 @@
 #include <cstdio>
+#include <rclcpp/rclcpp.hpp>
+
+using std::placeholders::_1;
+
+class DominicNode : public rclcpp::Node {
+
+public:
+  DominicNode() : Node("DominicNode") {
+    printf("hello world dominic_package package\n");
+  }
+};
 
 int main(int argc, char ** argv)
 {
   (void) argc;
   (void) argv;
 
-  printf("hello world dominic_package package\n");
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<DominicNode>());
+  rclcpp::shutdown();
+
   return 0;
 }
