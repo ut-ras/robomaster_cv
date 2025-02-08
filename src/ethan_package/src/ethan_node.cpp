ae8878944608f63da97e5a4c31f4d276c028b4d0
Ethan <ethan.present2005@gmail.com>
2025-02-07 18:42:47 -0600

    ethan onboarding: Spinning Node Complete

--- src/ethan_package/src/ethan_node.cpp
+++ src/ethan_package/src/ethan_node.cpp
@@ -1,10 +1,24 @@
 #include <cstdio>
+#include <rclcpp/rclcpp.hpp>
+
+
+class PreprocessingNode : public rclcpp::Node{
+  public: 
+    PreprocessingNode() : Node("preprocessing_node"){
+      printf("hello world ethan_package package\n");
+    }
+};
+
 
 int main(int argc, char ** argv)
 {
   (void) argc;
   (void) argv;
 
-  printf("hello world ethan_package package\n");
+  rclcpp::init(argc, argv);
+  // running an instance of the class forever (or until the user stops the script)
+  rclcpp::spin(std::make_shared<PreprocessingNode>());
+  rclcpp::shutdown();
+
   return 0;
 }
