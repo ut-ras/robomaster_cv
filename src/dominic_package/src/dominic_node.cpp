c6c3c39ba6f07a0aac749a06d90733d93a03e5ca
Dominic Paruolo <dominicparuolo78@gmail.com>
2025-02-07 18:55:08 -0600

    subscribed

--- src/dominic_package/src/dominic_node.cpp
+++ src/dominic_package/src/dominic_node.cpp
@@ -1,5 +1,7 @@
 #include <cstdio>
 #include <rclcpp/rclcpp.hpp>
+#include <sensor_msgs/msg/image.hpp>
+
 
 using std::placeholders::_1;
 
@@ -8,7 +10,15 @@ class DominicNode : public rclcpp::Node {
 public:
   DominicNode() : Node("DominicNode") {
     printf("hello world dominic_package package\n");
+    subscription_ = this->create_subscription<sensor_msgs::msg::Image>
+    ( "image_raw", 10, std::bind(&DominicNode::image_callback, this, _1));
   }
+private: 
+  void image_callback(const sensor_msgs::msg::Image & msg) const
+    {
+      RCLCPP_INFO(this->get_logger(), "I heard: '%d %d'", msg.width, msg.height);
+    }
+    rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr subscription_;
 };
 
 int main(int argc, char ** argv)
