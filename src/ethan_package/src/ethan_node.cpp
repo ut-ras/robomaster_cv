b4176b61cf3800205cd592b1b27cf208852e7fad
Ethan <ethan.present2005@gmail.com>
2025-02-14 16:54:56 -0600

    Listening to a topic completed

--- src/ethan_package/src/ethan_node.cpp
+++ src/ethan_package/src/ethan_node.cpp
@@ -1,12 +1,27 @@
 #include <cstdio>
 #include <rclcpp/rclcpp.hpp>
+#include <sensor_msgs/msg/image.hpp>
+using std::placeholders::_1;
 
 
 class PreprocessingNode : public rclcpp::Node{
   public: 
     PreprocessingNode() : Node("preprocessing_node"){
       printf("hello world ethan_package package\n");
+
+      // create a subscriber 
+      subscription_ = this->create_subscription<sensor_msgs::msg::Image>(
+        "image_raw", 10, std::bind(&PreprocessingNode::topic_callback, this, _1));
     }
+
+  private:
+  // method will run anytime a message is recieved from the publisher
+  void topic_callback(const sensor_msgs::msg::Image &msg) const {
+    //printing out the data
+    RCLCPP_INFO(this->get_logger(), "Message recieved: width: %d, height: %d\n", msg.width, msg.height);
+  }
+  // creating subscription variable 
+  rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr subscription_;
 };
 
 
