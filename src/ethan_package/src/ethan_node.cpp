48ea119b1fc4e5955f5195c4e6992764490c9e82
Ethan <ethan.present2005@gmail.com>
2025-02-14 18:19:50 -0600

    finished openCV

--- src/ethan_package/src/ethan_node.cpp
+++ src/ethan_package/src/ethan_node.cpp
@@ -1,6 +1,12 @@
 #include <cstdio>
 #include <rclcpp/rclcpp.hpp>
 #include <sensor_msgs/msg/image.hpp>
+//#include <sensor_msgs/image_encodings.h>
+#include <cv_bridge/cv_bridge.h>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+
 using std::placeholders::_1;
 
 
@@ -15,13 +21,48 @@ class PreprocessingNode : public rclcpp::Node{
     }
 
   private:
-  // method will run anytime a message is recieved from the publisher
-  void topic_callback(const sensor_msgs::msg::Image &msg) const {
-    //printing out the data
-    RCLCPP_INFO(this->get_logger(), "Message recieved: width: %d, height: %d\n", msg.width, msg.height);
-  }
-  // creating subscription variable 
-  rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr subscription_;
+    // method will run anytime a message is recieved from the publisher
+    void topic_callback(const sensor_msgs::msg::Image &msg) const {
+      //printing out the data
+      RCLCPP_INFO(this->get_logger(), "Message recieved: width: %d, height: %d\n", msg.width, msg.height);
+      imageCb(msg);
+    }
+    // creating subscription variable 
+    rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr subscription_;
+  
+  private: 
+    // function to convert and draw on image
+    void imageCb(const sensor_msgs::msg::Image &msg) const {
+      cv_bridge::CvImagePtr cv_ptr; 
+
+      try{
+        // making a copy of the image to modify 
+        cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+
+      }catch(cv_bridge::Exception &e){
+        printf("cv_brdige exception: %s", e.what());
+        return; 
+      }
+
+      // writing old image to file
+      cv::imwrite("old_image.jpeg", cv_ptr->image);
+
+      // draw somthing on the image
+      // circle(img, location, size, color)
+      cv::Mat mat; 
+      cv::cvtColor(cv_ptr->image, mat, cv::COLOR_BGR2GRAY);
+      cv::circle(cv_ptr->image, cv::Point(400, 200), 100, cv::Scalar(255, 0, 0));
+      cv::circle(cv_ptr->image, cv::Point(400, 400), 100, cv::Scalar(0, 255, 0));
+
+      cv::circle(mat, cv::Point(400, 200), 100, cv::Scalar(255, 0, 0));
+      cv::circle(mat, cv::Point(400, 400), 100, cv::Scalar(0, 255, 0));
+
+      
+
+      // writing new image to file 
+      cv::imwrite("new_image.jpeg", cv_ptr->image);
+      cv::imwrite("mat_image.jpeg", mat); 
+    }
 };
 
 
