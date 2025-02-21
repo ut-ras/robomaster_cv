73dad9dd98cad33e41f75c66896c7603fc4990c2
Braden McElroy <braden.mcelroy@utexas.edu>
2025-02-21 23:35:11 +0000

    my node

--- src/braden_package/src/braden_node.cpp
+++ src/braden_package/src/braden_node.cpp
@@ -1,10 +1,22 @@
 #include <cstdio>
+#include <iostream>
+#include <rclcpp/rclcpp.hpp>
+#include <std_msgs/msg/string.hpp>
+
+class BradenClass : public rclcpp::Node {
+public: 
+  BradenClass() : Node("braden_node") {
+    printf("Hello World braden_package package\n");
+  }
+};
 
 int main(int argc, char ** argv)
 {
   (void) argc;
   (void) argv;
 
-  printf("hello world braden_package package\n");
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<BradenClass>());
+  rclcpp::shutdown();
   return 0;
 }
