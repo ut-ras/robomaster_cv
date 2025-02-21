8704238240fba4931080480095ae186022b017e3
Isaac Choi <isaacchoi1014@gmail.com>
2025-02-21 22:47:16 +0000

    Added image subscriber

--- src/isaac_package/src/isaac_node.cpp
+++ src/isaac_package/src/isaac_node.cpp
@@ -1,10 +1,28 @@
-#include <cstdio>
+#include "rclcpp/rclcpp.hpp"
+#include "sensor_msgs/msg/image.hpp"
 
-int main(int argc, char ** argv)
+class ImageSubscriber : public rclcpp::Node
 {
-  (void) argc;
-  (void) argv;
+public:
+    ImageSubscriber() : Node("image_subscriber")
+    {
+        subscription_ = this->create_subscription<sensor_msgs::msg::Image>(
+            "/image_raw", 10, std::bind(&ImageSubscriber::imageCallback, this, std::placeholders::_1));
+    }
 
-  printf("hellooo world isaac_package package\n");
-  return 0;
+private:
+    void imageCallback(const sensor_msgs::msg::Image::SharedPtr msg)
+    {
+        RCLCPP_INFO(this->get_logger(), "Received Image: Width = %d, Height = %d", msg->width, msg->height);
+    }
+
+    rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr subscription_;
+};
+
+int main(int argc, char **argv)
+{
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<ImageSubscriber>());
+    rclcpp::shutdown();
+    return 0;
 }
