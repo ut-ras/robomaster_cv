6c61c916b42a80367b4bacfb5b9bc3f5c7d6db85
Isaac Choi <isaacchoi1014@gmail.com>
2025-02-21 23:20:10 +0000

    Added OpenCV image processing node with ROS2 integration

--- src/isaac_package/src/isaac_node.cpp
+++ src/isaac_package/src/isaac_node.cpp
@@ -1,28 +1,51 @@
-#include "rclcpp/rclcpp.hpp"
-#include "sensor_msgs/msg/image.hpp"
+#include <rclcpp/rclcpp.hpp>
+#include <sensor_msgs/msg/image.hpp>
+#include <cv_bridge/cv_bridge.h>
+#include <opencv2/opencv.hpp>
 
-class ImageSubscriber : public rclcpp::Node
-{
-public:
-    ImageSubscriber() : Node("image_subscriber")
-    {
-        subscription_ = this->create_subscription<sensor_msgs::msg::Image>(
-            "/image_raw", 10, std::bind(&ImageSubscriber::imageCallback, this, std::placeholders::_1));
-    }
+class ImageProcessor : public rclcpp::Node {
+  public:
+      ImageProcessor() : Node("image_processor") {
+          subscription_ = this->create_subscription<sensor_msgs::msg::Image>(
+              "/image_raw", 10, std::bind(&ImageProcessor::imageCallback, this, std::placeholders::_1));
 
-private:
-    void imageCallback(const sensor_msgs::msg::Image::SharedPtr msg)
-    {
-        RCLCPP_INFO(this->get_logger(), "Received Image: Width = %d, Height = %d", msg->width, msg->height);
-    }
+          publisher_ = this->create_publisher<sensor_msgs::msg::Image>("/processed_image", 10);
 
-    rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr subscription_;
-};
+          RCLCPP_INFO(this->get_logger(), "Image Processor Node Started...");
+      }
 
-int main(int argc, char **argv)
-{
-    rclcpp::init(argc, argv);
-    rclcpp::spin(std::make_shared<ImageSubscriber>());
-    rclcpp::shutdown();
-    return 0;
-}
+  private:
+      void imageCallback(const sensor_msgs::msg::Image::SharedPtr msg) {
+          try {
+              cv::Mat original_image = cv_bridge::toCvCopy(msg, "bgr8")->image;
+
+              cv::Mat gray_image, edges;
+              cv::cvtColor(original_image, gray_image, cv::COLOR_BGR2GRAY);
+              cv::Canny(gray_image, edges, 50, 150);
+
+              cv::imwrite("/robomaster_cv/original_image.jpg", original_image);
+              cv::imwrite("/robomaster_cv/processed_image.jpg", edges);
+
+              RCLCPP_INFO(this->get_logger(), "Saved images: original and processed.");
+
+              cv_bridge::CvImage processed_msg;
+              processed_msg.header = msg->header;
+              processed_msg.encoding = "mono8";  
+              processed_msg.image = edges;
+
+              publisher_->publish(*processed_msg.toImageMsg());
+          }
+          catch (const cv_bridge::Exception &e) {
+              RCLCPP_ERROR(this->get_logger(), "cv_bridge exception: %s", e.what());
+          }
+      }
+      rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr subscription_;
+      rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr publisher_;
+  };
+
+  int main(int argc, char **argv) {
+      rclcpp::init(argc, argv);
+      rclcpp::spin(std::make_shared<ImageProcessor>());
+      rclcpp::shutdown();
+      return 0;
+  }
